abe9957ac026f6295ee9123ef09b3731a1ca9917
NikolaiAtanasoski <65742065+NikolaiAtanasoski@users.noreply.github.com>
2020-12-15 16:44:19 +0100

    Day 9

--- src/day9/day9.py
+++ src/day9/day9.py
@@ -14,29 +14,54 @@ class Day9:
         data = []
         
         for line in self.input_data:
-            data.append(line)
+            data.append(int(line))
 
         return data
 
-    def puzzle1(self):
-        data = self.format_input()
+    def puzzle1(self, preamble_length):
+        numbers = self.format_input()
 
 
+        for i in range(preamble_length,len(numbers) - 1):
+            print(f"next number: {numbers[i]}")
+            possible_next_numbers = self.possible_results(numbers[i-preamble_length:i])
+            if numbers[i] not in possible_next_numbers:
+                return numbers[i]
+            
 
-        return data
+        return None
+    
+    def possible_results(self, numbers):
+        possible_results = list()
+        for i in range(len(numbers)):
+            for j in range(i+1,len(numbers)):
+                possible_results.append(numbers[i]+numbers[j])
+          
+        print(f"Numbers: {numbers} results: {possible_results}\n")
+        return possible_results
+    
+    def puzzle2(self, number):
+        numbers = self.format_input()
 
-    def puzzle2(self):
-        data = self.format_input()
+        for i in range(len(numbers)):
+            result = numbers[i]
+            result_set = {numbers[i]}
+            for j in range(i+1,len(numbers)):
+                result += numbers[j]
+                result_set.add(numbers[j])
+                if result == number:
+                    return max(result_set) + min(result_set)
+                elif result > number:
+                    break
 
+        return None
 
-
-        return data
 
 
 day9 = Day9()
 
-answer1 = day9.puzzle1()
-print(f"Answer 1 : {answer1}")
+# answer1 = day9.puzzle1(5)
+# print(f"Answer 1 : {answer1}")
 
-answer2 = day9.puzzle2()
+answer2 = day9.puzzle2(133015568)
 print(f"Answer 2 : {answer2}")
