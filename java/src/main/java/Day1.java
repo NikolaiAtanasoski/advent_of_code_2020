32736d8a6bb9a7b15f23740cfcbe6943a66aefff
NikolaiAtanasoski <nikolai.atanasoski@gmail.com>
2021-03-26 17:13:31 +0100

    day1 puzzle 1 JAVA

--- java/src/main/java/Day1.java
+++ java/src/main/java/Day1.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day1 {
 
@@ -8,11 +9,39 @@ public class Day1 {
     }
 
     public void run() {
-        List<String> content = InputReader.readFileFromResourcesIntoList("testfile.txt");
+        List<String> content = InputReader.readFileFromResourcesIntoList("day1_input.txt");
+        List<Integer> numbers = content.stream().map(Integer::valueOf).collect(Collectors.toList());
 
-        for(String line : content){
-            System.out.println(line);
+        solvePuzzle1(numbers);
+    }
+
+    public void solvePuzzle1(List<Integer> numbers) {
+        numbers.sort(Integer::compare);
+
+        String answerToPuzzle1 = "";
+        for (int i = 0; i < numbers.size(); i++) {
+
+            int number1 = numbers.get(i);
+
+            for (int j = i + 1; j < numbers.size(); j++) {
+
+                int number2 = numbers.get(j);
+                int result = number1 + number2;
+
+                if (result == 2020) {
+                    answerToPuzzle1 = "Answer is: " + number1 + " * " + number2 + (number1 * number2);
+                    break;
+                } else if (result > 2020) {
+                    break;
+                }
+            }
+
+            if (!answerToPuzzle1.isEmpty()) {
+                break;
+            }
         }
+
+        System.out.println(answerToPuzzle1);
     }
 
 }
